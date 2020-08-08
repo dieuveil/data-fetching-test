602e33e0ddff1ddf6e566c568add32a517759860
dieuveil <dieuveilmabirou@gmail.com>
2020-08-08 11:41:40 +0200

    Updates

---
+++ precache-manifest.57f037023b9ea55305ef5b2987b07906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2061d20fa58bd03a194fa1f6eecdf5",
+    "url": "/data-fetching-test/index.html"
+  },
+  {
+    "revision": "071b728baccbe5bba667",
+    "url": "/data-fetching-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb67d56ae7d816ba3fbb",
+    "url": "/data-fetching-test/static/js/2.a9fa3ca5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data-fetching-test/static/js/2.a9fa3ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071b728baccbe5bba667",
+    "url": "/data-fetching-test/static/js/main.b757c23d.chunk.js"
+  },
+  {
+    "revision": "9067e12dbc10083c55c4",
+    "url": "/data-fetching-test/static/js/runtime-main.117c5c1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data-fetching-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
