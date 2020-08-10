8e7d74f1a54cc0afb0657100af82cad85d799622
dieuveil <dieuveilmabirou@gmail.com>
2020-08-10 10:16:40 +0200

    First test of the app with axios

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
-{/*
-const resultsStyle = {
-    padding: "20px"
-}*/}
 
-function Results(props) {
+const Results = (props) => {
+    const {repos} = props
+    //console.log(repos)
+    const listrepos = 
+    repos.length !== 0 ? (
+        //repos.data.slice(0,5).map((item) => <li key={item.id}>{item.name}</li>)
+        //repos.map((item) => <li key={item.id}>{item.name}</li>)
+        console.log(repos)
+    )
+    : (<h5>No data found</h5>);
     return (
           <ul>
-            <li>First repo</li>
+           { /*{
+                repos.length !== 0 ? (
+                    
+                    repos.data.map((item) => <li key={item.id}>{item.name}</li>)
+                )
+                : (<h5>No data found</h5>)
+            }*/}
+            {listrepos}
           </ul> 
+         
     );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Results from './Results'
+import axios from 'axios'
 
 
 function SearchBar(props) {
-    const [searchInput, setsearchInput] = useState('')
+    const [searchInput, setsearchInput] = useState("")
+    const [repos, setRepos] = useState([])
 
     const handleChange = (e) =>{
-        setsearchInput(e.target.value)
-    }
+        //setsearchInput(e.target.value)
+        setsearchInput(e.target.value.substr(0,20))
+    };
 
-    const handleClick = () =>{
-        console.log(searchInput)
-    }
+   /*const handleClick = async () => { 
+          try{
+            //const result = await axios(`https://api.github.com/repos/dieuveil/data-fetching-test`)
+            const result = await axios(`https://api.github.com/users/dieuveil/repos`)
+            setRepos(result.data)
+          }catch(err){
+            console.log(err)
+          }
+    };*/
+
+    useEffect(()=>{
+        axios
+            .get('https://api.github.com/users/dieuveil/repos')
+            .then(res =>{
+                console.log(res)
+                setRepos(res.data)
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    })
+
+    let filteredRepos = repos.filter(
+        (repo) =>{
+            return repo.name.indexOf(searchInput) !== -1
+        }
+    )
+   
     return (
         <div>
             <input type="text" placeholder="Search" value={searchInput} onChange={handleChange} />
-            <button onClick={handleClick}>Search</button>
+            {/*<button onClick={handleClick}>Search</button>
+            <Results repos={repos}/>*/}
+            <ul>
+                {
+                    filteredRepos.map(
+                    (repo)=><li key={repo.id}>{repo.name}</li>
+                    )
+                }
+            </ul>
+            
         </div>
     );
 }
